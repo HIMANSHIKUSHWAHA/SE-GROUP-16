1749d82dd9369ccf1e9550046c21197f335002f7
rahulgupta046 <rg913000@gmail.com>
2023-09-30 14:33:10 -0400

    directory structure changed

--- ui/src/containers/auth/clientSignup.js
+++ ui/src/containers/auth/clientSignup.js
@@ -4,14 +4,14 @@ import validator from "validator";
 import { postClientSignUp } from "../api";
 
 export default function ClientSignup(props) {
-    
+
     const [formData, setFormData] = useState({
-        firstName:"",
-        lastName:"",
-        role:"",
-        email:"",
-        password:"",
-        rePassword:""
+        firstName: "",
+        lastName: "",
+        role: "client",
+        email: "",
+        password: "",
+        rePassword: ""
     });
 
     const [strengthMessage, setStrengthMessage] = useState(null);
@@ -19,9 +19,9 @@ export default function ClientSignup(props) {
     const [validEmail, setValidEmail] = useState(null);
 
     const handleInputChange = (event) => {
-        const {name, value} = event.target;
+        const { name, value } = event.target;
         setFormData((prevFormData) => {
-            return {...prevFormData, [name]: value};
+            return { ...prevFormData, [name]: value };
         });
         setStrengthMessage(null);
         setSamePass(null);
@@ -30,25 +30,25 @@ export default function ClientSignup(props) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formData);     
+        console.log(formData);
 
-        if(validator.isEmail(formData.email)){
-            if(validator.isStrongPassword(formData.password, {
+        if (validator.isEmail(formData.email)) {
+            if (validator.isStrongPassword(formData.password, {
                 minLength: 8,
                 minLowercase: 1,
                 minUppercase: 1,
                 minNumbers: 1,
                 minSymbols: 1
-            })){
-                if(formData.password === formData.rePassword){
+            })) {
+                if (formData.password === formData.rePassword) {
                     postClientSignUp(formData);
-                }else{
+                } else {
                     setSamePass("The passwords does not match");
                 }
-            }else {
+            } else {
                 setStrengthMessage("Password should contain atleast 8 characters \n 1 lowercase letter \n 1 uppercase letter \n 1 numeric value and 1 symbol");
             }
-        }else {
+        } else {
             setValidEmail("Not a valid email");
         }
     };
@@ -66,83 +66,83 @@ export default function ClientSignup(props) {
                     <div className="form-group mt-3">
                         <label>First Name</label>
                         <input
-                        type="text"
-                        className="form-control mt-1"
-                        placeholder="e.g Rohit"
-                        name="firstName"
-                        onChange={handleInputChange}
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="e.g Rohit"
+                            name="firstName"
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="form-group mt-3">
                         <label>Last Name</label>
                         <input
-                        type="text"
-                        className="form-control mt-1"
-                        placeholder="e.g Sharma"
-                        name="lastName"
-                        onChange={handleInputChange}
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="e.g Sharma"
+                            name="lastName"
+                            onChange={handleInputChange}
                         />
                     </div>
                     <div className="form-group mt-3">
                         <label>How do you want to Sign Up</label>
-                        <select 
-                          className="form-control mt-1"
-                          name="role"
-                          value={formData.role} 
-                          onChange={handleInputChange}
+                        <select
+                            className="form-control mt-1"
+                            name="role"
+                            value={formData.role}
+                            onChange={handleInputChange}
                         >
-                          <option value="client">Client</option>
-                          <option value="professional">Professional</option>
+                            <option value="client">Client</option>
+                            <option value="professional">Professional</option>
                         </select>
                     </div>
                     <div className="form-group mt-3">
                         <label>Email address</label>
                         <input
-                        type="text"
-                        className="form-control mt-1"
-                        placeholder="e.g abcd@example.com"
-                        name="email"
-                        onChange={handleInputChange}
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="e.g abcd@example.com"
+                            name="email"
+                            onChange={handleInputChange}
                         />
                     </div>
                     {validEmail === null ? null :
-                    <span style={{
-                        color: 'darkred',
-                        fontSize: 13,
-                    }}>{validEmail}</span>}
+                        <span style={{
+                            color: 'darkred',
+                            fontSize: 13,
+                        }}>{validEmail}</span>}
                     <div className="form-group mt-3">
                         <label>New Password</label>
                         <input
-                        type="password"
-                        className="form-control mt-1"
-                        placeholder="Password"
-                        name="password"
-                        onChange={handleInputChange}
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Password"
+                            name="password"
+                            onChange={handleInputChange}
                         />
                     </div>
                     {strengthMessage === null ? null :
-                    <span style={{
-                        color: 'darkred',
-                        fontSize: 13,
-                    }}>{strengthMessage}</span>}
+                        <span style={{
+                            color: 'darkred',
+                            fontSize: 13,
+                        }}>{strengthMessage}</span>}
                     <div className="form-group mt-3">
                         <label>Confirm Password</label>
                         <input
-                        type="password"
-                        className="form-control mt-1"
-                        placeholder="Comfirm Password"
-                        name="rePassword"
-                        onChange={handleInputChange}
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Comfirm Password"
+                            name="rePassword"
+                            onChange={handleInputChange}
                         />
                     </div>
                     {samePass === null ? null :
-                    <span style={{
-                        color: 'darkred',
-                        fontSize: 13,
-                    }}>{samePass}</span>}
+                        <span style={{
+                            color: 'darkred',
+                            fontSize: 13,
+                        }}>{samePass}</span>}
                     <div className="d-grid gap-2 mt-3">
                         <button type="submit" className="btn btn-primary">
-                        Sign me up!!
+                            Sign me up!!
                         </button>
                     </div>
                     <p className="text-center mt-2">
