0c83471f8a64f9d7324235b4803ba33252924942
rahulgupta046 <rg913000@gmail.com>
2023-10-10 18:48:32 -0400

    basic dashboard UI set

--- ui/src/containers/dashboard.js
+++ ui/src/containers/dashboard.js
@@ -1,11 +1,62 @@
-import React from 'react';
+import React, { useState } from "react";
+import Header from "./header";
+import FilterSearchSection from "./searchbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVideo, faRunning, faUtensils, faDumbbell, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
 
-export default function Dashboard() {
 
-  return(
+// Secondary Navbar Component
+const SecondaryNavbar = () => (
+  <div className="SecondaryNavbar">
+    <button>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faVideo} size="1x" />
+        <div>Workout Videos</div>
+      </div>
+    </button>
+    <button>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faRunning} size="1x" />
+        <div>Workout Programs</div>
+      </div>
+    </button>
+    <button>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faUtensils} size="1x" />
+        <div>Meal Plans</div>
+      </div>
+    </button>
+    <button>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faDumbbell} size="1x" />
+        <div>Custom Workouts</div>
+      </div>
+    </button>
+    <button>
+      <div className="icon-container">
+        <FontAwesomeIcon icon={faCalendarCheck} size="1x" />
+        <div>Routines</div>
+      </div>
+    </button>
+  </div>
+);
 
-    <h2>Dashboard</h2>
+// Content Area Component
+const ContentArea = () => (
+  <div className="ContentArea">
+    {/* Placeholder for workout video cards */}
+    <p>Workout Video Cards will be displayed here</p>
+  </div>
+);
 
-  );
+// Dashboard Component
+const Dashboard = () => (
+  <div>
+    <Header />
+    <FilterSearchSection />
+    <SecondaryNavbar />
+    <ContentArea />
+  </div>
+);
 
-}
+export default Dashboard;
