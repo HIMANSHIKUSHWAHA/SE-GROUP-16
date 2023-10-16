fbc9b82c38c2c3adbda9fa13077aa42d6cdafdaa
rahulgupta046 <rg913000@gmail.com>
2023-10-15 21:16:13 -0400

    calendar

--- ui/src/containers/dashboard.js
+++ ui/src/containers/dashboard.js
@@ -3,36 +3,36 @@ import Header from "./header";
 import FilterSearchSection from "./searchbar";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faVideo, faRunning, faUtensils, faDumbbell, faCalendarCheck } from '@fortawesome/free-solid-svg-icons';
-
+import CalendarView from "./dashboard/CalendarViewComponent/CalendarView";
 
 // Secondary Navbar Component
-const SecondaryNavbar = () => (
+const SecondaryNavbar = ({ setActiveContent }) => (
   <div className="SecondaryNavbar">
-    <button>
+    <button onClick={() => setActiveContent('videos')}>
       <div className="icon-container">
         <FontAwesomeIcon icon={faVideo} size="1x" />
         <div>Workout Videos</div>
       </div>
     </button>
-    <button>
+    <button onClick={() => setActiveContent('workout-programs')}>
       <div className="icon-container">
         <FontAwesomeIcon icon={faRunning} size="1x" />
         <div>Workout Programs</div>
       </div>
     </button>
-    <button>
+    <button onClick={() => setActiveContent('meal-plans')}>
       <div className="icon-container">
         <FontAwesomeIcon icon={faUtensils} size="1x" />
         <div>Meal Plans</div>
       </div>
     </button>
-    <button>
+    <button onClick={() => setActiveContent('custom-workouts')}>
       <div className="icon-container">
         <FontAwesomeIcon icon={faDumbbell} size="1x" />
         <div>Custom Workouts</div>
       </div>
     </button>
-    <button>
+    <button onClick={() => setActiveContent('routines')}>
       <div className="icon-container">
         <FontAwesomeIcon icon={faCalendarCheck} size="1x" />
         <div>Routines</div>
@@ -42,21 +42,27 @@ const SecondaryNavbar = () => (
 );
 
 // Content Area Component
-const ContentArea = () => (
+const ContentArea = ({ activeContent }) => (
   <div className="ContentArea">
-    {/* Placeholder for workout video cards */}
-    <p>Workout Video Cards will be displayed here</p>
+    {activeContent === 'videos' && <p>Workout Video Cards will be displayed here</p>}
+    {activeContent === 'routines' && <CalendarView />}
+    {activeContent === 'workout-programs' && <p>workout programs data</p>}
+    {activeContent === 'meal-plans' && <p>meal plans data</p>}
+    {activeContent === 'custom-workouts' && <p>custom workouts data</p>}
   </div>
 );
 
 // Dashboard Component
-const Dashboard = () => (
-  <div>
-    <Header />
-    <FilterSearchSection />
-    <SecondaryNavbar />
-    <ContentArea />
-  </div>
-);
+const Dashboard = () => {
+  const [activeContent, setActiveContent] = useState('videos'); // Default content
+  return (
+    <div>
+      <Header />
+      <FilterSearchSection />
+      <SecondaryNavbar setActiveContent={setActiveContent} />
+      <ContentArea activeContent={activeContent} />
+    </div>
+  );
+};
 
 export default Dashboard;
