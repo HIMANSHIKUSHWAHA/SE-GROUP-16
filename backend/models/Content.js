63a5edc3936d13a185f1812eb0a2623c6cc3a781
Juntao Chi <chijun@iu.edu>
2023-10-29 18:50:07 -0400

    updated subscribe controller

--- backend/models/Content.js
+++ backend/models/Content.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const contentSchema = new mongoose.Schema({
+    title: {
+        type: String
+    },
+    description : {
+        type: String
+    },
     creator: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
