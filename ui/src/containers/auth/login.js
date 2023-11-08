649826a9990effdf442c5bee913c1a4f83ef7350
NATHAN <whipmiester@hotmail.com>
2023-11-07 19:47:49 -0500

    'additional'

--- ui/src/containers/auth/login.js
+++ ui/src/containers/auth/login.js
@@ -80,7 +80,6 @@ export default function Login(props) {
                 setPassErr("An error occurred. Please try again.");
             }
         } catch (error) {
-            setNav("/2fa");
             console.error("Login request failed:", error);
             setAuthenticationStat(false);
             setPassErr("Failed to connect. Please check your internet connection and try again.");
