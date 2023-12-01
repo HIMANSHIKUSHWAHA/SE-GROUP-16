f1164b3410526270edbf40a19d09bdaf6d7b93fc
HIMANSHI KUSHWAHA <himanshikushwaha305@gmail.com>
2023-12-01 00:53:47 -0500

    created routes for request approvals

--- backend/models/Professional.js
+++ backend/models/Professional.js
@@ -1,86 +1,98 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const professionalSchema = new Schema({
-    firstName: {
-        type: String,
-        select: true,
-        required: true,
+  firstName: {
+    type: String,
+    select: true,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    select: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: true,
+  },
+  specialization: {
+    type: String,
+    required: true,
+    select: true,
+  },
+  resetPasswordToken: {
+    type: String,
+    select: false,
+  },
+  resetPasswordExpires: {
+    type: Date,
+    select: false,
+  },
+  twoFASecret: {
+    type: String,
+    select: false,
+  },
+  twoFAEnabled: {
+    type: Boolean,
+    default: false,
+    select: false,
+  },
+  otp: {
+    type: String,
+    select: true,
+  },
+  otpExpires: {
+    type: Date,
+    select: true,
+  },
+  Subscribers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      select: false,
     },
-    lastName: {
-        type: String,
-        select: true,
-        required: true,
+  ],
+  asyncVideosAssociated: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "AsyncVideo",
+      select: false,
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        lowercase: true,
+  ],
+  LiveSessionCreated: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "LiveSession",
+      select: false,
     },
-    password: {
-        type: String,
-        required: true,
-        select: true,
-    },
-    specialization: {
-        type: String,
-        required: true,
-        select: true,
-    },
-    resetPasswordToken: {
-        type: String,
-        select: false,
-    },
-    resetPasswordExpires: {
-        type: Date,
-        select: false,
-    },
-    twoFASecret: {
-        type: String,
-        select: false
-    },
-    twoFAEnabled: {
-        type: Boolean,
-        default: false,
-        select: false
-    },
-    otp: {
-        type: String,
-        select: true
-    },
-    otpExpires: {
-        type: Date,
-        select: true
-    },
-    Subscribers: [{
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        select: false
-    }],
-    asyncVideosAssociated: [{
-        type: Schema.Types.ObjectId,
-        ref: 'AsyncVideo',
-        select: false
-    }],
-    LiveSessionCreated: [{
-        type: Schema.Types.ObjectId,
-        ref: 'LiveSession',
-        select: false
-    }],
+  ],
+  requestQueue: {
+    type: Array,
+    select: false,
+  },
 });
 
-professionalSchema.pre('save', async function (next) {
-    if (this.isModified('password') || this.isNew) {
-        this.password = await bcrypt.hash(this.password, 12);
-    }
-    next();
+professionalSchema.pre("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
 });
 
-professionalSchema.methods.comparePassword = async function (candidatePassword) {
-    return await bcrypt.compare(candidatePassword, this.password);
+professionalSchema.methods.comparePassword = async function (
+  candidatePassword
+) {
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('Professional', professionalSchema);
+module.exports = mongoose.model("Professional", professionalSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const twoFARoutes = require('./routes/2faRoutes');
-const dashboardRoutes = require('./routes/dashboardRoutes');
-const searchRoutes = require('./routes/searchRoutes');
-const settingsRoutes = require('./routes/settingsRoutes');
-const errorHandler = require('./middleware/errorHandler');
-const session = require('express-session');
-const passport = require('./config/passport');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const twoFARoutes = require("./routes/2faRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
+const searchRoutes = require("./routes/searchRoutes");
+const settingsRoutes = require("./routes/settingsRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const session = require("express-session");
+const passport = require("./config/passport");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const Professional = require("../../models/Professional");
 
-console.log('Started backend server');
+console.log("Started backend server");
 
 // Connect to mongoose
 connectDB();
@@ -24,35 +26,136 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Session Middleware Initialization with secure cookie settingsTab
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',  // Ensure secure cookies in production
-        httpOnly: true,
-        sameSite: 'Strict'
-    }
-}));
+      secure: process.env.NODE_ENV === "production", // Ensure secure cookies in production
+      httpOnly: true,
+      sameSite: "Strict",
+    },
+  })
+);
 
 // Initialize Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/auth/2fa', twoFARoutes);
-app.use('/api/v1/dashboard', dashboardRoutes)
-app.use('/api/v1/search', searchRoutes);
-app.use('/api/v1/settings', settingsRoutes);
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/auth/2fa", twoFARoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/search", searchRoutes);
+app.use("/api/v1/settings", settingsRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 // Error handler to be placed last
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+/*
+Request Queue
+
+Proffestional Trainer Side id(9012)
+VideoId  DateRequested TimeRequested Status
+re34r3r  12/12/2020      12:00       Pending
+
+requestQueue = [{
+    videoId: "re34r3r",
+    dateRequested: "12/12/2020",
+    timeRequested: "12:00",
+    status: "Pending"
+}]
+
+admin approves the request
+update adminsRequestQueue in mongodb
+update requestQueue in mongodb
+
+first update the adminsRequestQueue in mongodb status to approved
+
+For updating proffestional side
+
+fetch adminsRequestQueue from mongodb
+find your id in the adminsRequestQueue
+check if status is approved
+requestQueue update that videoId status to approved
+*/
+
+// Recieving Requests from the proffestional trainer
+app.get("/request-admin", (req, res) => {
+  let proffestionalId = req.body.proffestionalId;
+  let videoRequest = req.body.videoRequest;
+  let date = req.body.date;
+  let time = req.body.time;
+  let status = req.body.status;
+  let videoId = req.body.videoId;
+  let requestQueue = {
+    proffestionalId: proffestionalId,
+    videoId: videoId,
+    dateRequested: date,
+    timeRequested: time,
+    status: status,
+    videoRequest: videoRequest,
+  };
+  Professional.findOneAndUpdate(
+    { _id: proffestionalId },
+    { $push: { requestQueue: requestQueue } }
+  );
+});
+
+app.get("/retrieve-all-requests", (req, res) => {
+  Professional.find({
+    requestQueue: { $exists: true, $ne: [] },
+  }).then((data) => {
+    res.send(data);
+  }
+);
+
+app.get("/approve-request",(req,res)=>{
+    let videoId = req.body.videoId;
+    let proffestionalId = req.body.proffestionalId;
+    let proffestionalExists = Professional.findOne({_id: proffestionalId});
+    if(!proffestionalExists){
+        res.send("Proffestional does not exist");
+    }
+    Professional.findOneAndUpdate(
+        {_id: proffestionalId},
+        {$set: {"requestQueue.$[element].status": "Approved"}},
+        {arrayFilters: [{"element.videoId": videoId}]}
+    )
+})
+
+app.get("/decline-request",(req,res)=>{
+    let videoId = req.body.videoId;
+    let proffestionalId = req.body.proffestionalId;
+    let proffestionalExists = Professional.findOne({_id: proffestionalId});
+    if(!proffestionalExists){
+        res.send("Proffestional does not exist");
+    }
+    Professional.findOneAndUpdate(
+        {_id: proffestionalId},
+        {$set: {"requestQueue.$[element].status": "Declined"}},
+        {arrayFilters: [{"element.videoId": videoId}]}
+    )
+})
+
+app.get("delete-approved-request",(req,res)=>{
+    let videoId = req.body.videoId;
+    let proffestionalId = req.body.proffestionalId;
+    let proffestionalExists = Professional.findOne({_id: proffestionalId});
+    if(!proffestionalExists){
+        res.send("Proffestional does not exist");
+    }
+    Professional.findOneAndUpdate(
+        {_id: proffestionalId},
+        {$pull: {requestQueue: {videoId: videoId}}}
+    )
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(Server is running on http://localhost:${PORT});
 });
