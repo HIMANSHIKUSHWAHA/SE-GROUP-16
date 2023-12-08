495b5279106b2e60eab9ea066452dc29df2edbcc
aveldan <aveldan@iu.edu>
2023-12-08 17:45:14 -0500

    something

--- ui/src/containers/dashboard/VideoTab/videoSearchTab.js
+++ ui/src/containers/dashboard/VideoTab/videoSearchTab.js
@@ -1,11 +1,9 @@
 import React, {useState, useEffect, useContext} from 'react';
-import React, {useState, useEffect, useContext} from 'react';
 import axios from 'axios';
 import VideoPreview from './../videoEmbeds';
 import { Box, TextField, Button, List, ListItem, Typography, Paper, Grid, Card, CardContent } from '@mui/material';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import {UserContext} from "../../../context";
-import RatingsComponent from "../RatingsButtons/RatingsComponent";
 
 const VideoSearch = () => {
     const [searchTerm, setSearchTerm] = useState('');
